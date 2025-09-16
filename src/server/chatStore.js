fe522dcae10c30fc60a9d50136cffc65ecc5032f
ForestMars <themarsgroup+gh@gmail.com>
2025-09-16 17:18:16 -0400

    make chatStore robust

--- src/server/chatStore.js
+++ src/server/chatStore.js
@@ -1,10 +1,9 @@
 // src/server/chatStore.js
-// Simple JSON file-based chat store for prototyping (ESM)
+// Simple JSON file-based chat store for prototyping
 import fs from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
-// __dirname replacement for ESM
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
