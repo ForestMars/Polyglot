9af78376419374c87fd203694641cb60eb85905e
ForestMars <themarsgroup+gh@gmail.com>
2025-10-03 00:21:50 -0400

    import order

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,9 @@
 // src/main.tsx
-
-
+import { createRoot } from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
 import { runClientMigrations } from './services/browserMigrations';
+
 // Run client DB migrations before app renders
 runClientMigrations().then(() => {
 	console.log('[migrations] Client migrations complete');
@@ -12,7 +14,3 @@ function startApp() {
 	// ...existing app startup code...
 	createRoot(document.getElementById("root")!).render(<App />);
 }
-
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
